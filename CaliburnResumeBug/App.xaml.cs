4ffc41bd4ed7360f87da607ed41a2c03ea5ea433
Peter Andersson <petoj87@gmail.com>
2015-12-01 22:53:17 +0100

    Initial commit

---
+++ CaliburnResumeBug/App.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Caliburn.Micro;
+using CaliburnResumeBug.Messages;
+using CaliburnResumeBug.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace CaliburnResumeBug
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : CaliburnApplication
+    {
+        private WinRTContainer _container;
+        private IEventAggregator _eventAggregator;
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            LogManager.GetLog = type => new DebugLog(type);
+            _container = new WinRTContainer();
+            _container.RegisterWinRTServices();
+
+            _container
+                .PerRequest<ShellViewModel>()
+                .PerRequest<TestViewModel>();
+            _eventAggregator = _container.GetInstance<IEventAggregator>();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+            if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                _eventAggregator.PublishOnUIThread(new ResumeStateMessage());
+        }
+
+        protected override void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            _eventAggregator.PublishOnUIThread(new SuspendStateMessage(e.SuspendingOperation));
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ CaliburnResumeBug/ViewModels/ShellViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using CaliburnResumeBug.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace CaliburnResumeBug.ViewModels
+{
+    public class ShellViewModel : Screen, IHandle<ResumeStateMessage>, IHandle<SuspendStateMessage>
+    {
+        private bool resume;
+        private readonly WinRTContainer container;
+        private readonly IEventAggregator eventAggregator;
+        private INavigationService navigationService;
+
+        public ShellViewModel(WinRTContainer container, IEventAggregator eventAggregator)
+        {
+            this.container = container;
+            this.eventAggregator = eventAggregator;
+        }
+
+        protected override void OnActivate()
+        {
+            eventAggregator.Subscribe(this);
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            eventAggregator.Unsubscribe(this);
+        }
+
+        public void SetupNavigationService(Frame frame)
+        {
+            navigationService = container.RegisterNavigationService(frame);
+
+            if (resume)
+                navigationService.ResumeState();
+            else
+                navigationService.For<TestViewModel>().Navigate();
+        }
+
+        public void Handle(SuspendStateMessage message)
+        {
+            navigationService.SuspendState();
+        }
+
+        public void Handle(ResumeStateMessage message)
+        {
+            resume = true;
+        }
+    }
+}
